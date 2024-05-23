42c0470f0a3831907bf8507c52f85219b62ff046
DL23727272 <113783762+DL23727272@users.noreply.github.com>
2024-05-23 09:39:16 +0800

    updated conent

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,6 @@
-go_home
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.core.window import Window
+from kivy.uix.screenmanager import Screen
+from kivy.uix.screenmanager import ScreenManager
+import subprocess
